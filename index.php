b68379c12115614632204babba82534a9fee5811
elboweer.Anton <elboweer@gmail.com>
2018-10-29 16:23:52 +0300

    Download Front-End Part

--- index.php
+++ index.php
@@ -1,1 +1,105 @@
-<?php echo 'test';
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>TEST</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../css/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="../css/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+    <script src="https://use.fontawesome.com/f8ccf15fdc.js"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+<? 
+	/* -- Построение шапки -- */
+	require('php/main/header.php'); 
+	/* -- Построение сайд меню -- */
+	require('php/main/side_navigation.php');
+?>
+
+        <div id="page-wrapper">
+        	<div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Сводная информация</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+<?
+	require('php/page/page_maintop_header.php');
+?>
+            <div class="row">
+                <div class="col-lg-8">
+<?
+	/* -- Запуск модулей -- */
+	require('php/modules/area_chart.php');
+	require('php/modules/bar_chart.php');
+	require('php/modules/timeline.php');
+?>  
+                </div>
+
+
+    <!-- Сайд модули-->
+                <div class="col-lg-4">
+<?
+	require('php/modules/notifications.php');
+	require('php/modules/donut_chart.php');
+	require('php/modules/chat.php');
+?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../js/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="../js/raphael.min.js"></script>
+    <script src="../js/morris.min.js"></script>
+    <script src="../js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
